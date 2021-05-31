696c9ceb2806ae31ae1d86ca4514af98f77564f0
Joshua Fletcher <joshua.f1998@gmail.com>
2021-05-31 20:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function writeCards(names, event) {
+  completeCards = []
   for (let i = 0; i < names.length; i++) {
-    i.push("Thank you, ${names[i]}, for the wonderful ${event[i]} gift!")
+    completeCards.push(`Thank you, ${names[i]}, for the wonderful ${event[i]} gift!`)
   }
   
 }
